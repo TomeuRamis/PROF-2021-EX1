760ed5aea6f0d0c2b7c945e661fcf3533b55bf70
TomeuRamis <tomeuramis99@yahoo.es>
2021-10-21 17:39:55 +0200

    initial commit

--- src/test/java/es/upm/grise/profundizacion/control_1/LibraryTest.java
+++ src/test/java/es/upm/grise/profundizacion/control_1/LibraryTest.java
@@ -1,10 +1,11 @@
 package es.upm.grise.profundizacion.control_1;
 
-public class LibraryTest {
-	
-	@Test
-	public void test() {
-		fail();
-	}
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
+public class LibraryTest {
+
+   
 }
